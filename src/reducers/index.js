2d83a3cc84d0999230f4d2808ec3e41f65f9fec7
Eric Cleek <ecleek@lakenetwork.net>
2019-03-09 22:38:08 -0500

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from "redux-form";
+import streamReducer from './StreamReducer'
+
+import authReducer from './authReducer';
+
+export default combineReducers({
+    form: formReducer,
+    auth: authReducer,
+    streams: streamReducer
+});
